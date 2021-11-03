cb59632933d848c560ce1f269792798a9e51cf24
Italo Borges <italoborgesevangelista@gmail.com>
2021-11-03 18:04:23 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -27,14 +28,22 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		System.out.println("=== TESTE 5: Seller Insert ===");
+		System.out.println("=== TESTE 4: Seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserido! Novo id = " + newSeller.getId());
+		System.out.println("=== TESTE 5: Seller UpDate ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		seller.setEmail("martha@gmail.com");
 		sellerDao.update(seller);
 		System.out.println("Update Concluido");
-
+		System.out.println("=== TESTE 6: Seller Delete ===");
+		System.out.println("Enter id for delete");
+		Scanner sc = new Scanner(System.in);
+		sellerDao.deleteById(sc.nextInt());
+		System.out.println("Delete computer");
+		sc.close();
 	}
 
 }
